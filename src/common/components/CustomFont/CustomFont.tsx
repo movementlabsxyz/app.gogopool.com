8e7365e5f858904406994816d630030b8b3c817a
Andre H <andrehadiantolesmana@gmail.com>
2022-07-05 01:12:51 +0800

    refactor: custom chakra-ui fonts

--- src/common/components/CustomFont/CustomFont.tsx
+++ src/common/components/CustomFont/CustomFont.tsx
@@ -1,3 +1,8 @@
+import { Global } from "@emotion/react";
+
+/**
+ * Custom Fonts for tailwindcss.config
+ */
 export const CustomFonts = (): JSX.Element => {
   return (
     <>
@@ -32,3 +37,61 @@ export const CustomFonts = (): JSX.Element => {
     </>
   );
 };
+
+/**
+ * Custom Fonts for Chakra-UI theme
+ */
+export const Fonts = {
+  heading: "'Domaine', sans-serif",
+  body: "'Jost', sans-serif",
+};
+
+export const ChakraFonts = () => (
+  <Global
+    styles={`
+    @font-face {
+      font-family: "Jost";
+      font-style: normal;
+      font-weight: normal;
+      font-display: swap;
+      src: url(/assets/fonts/Jost/Jost-VariableFont_wght.ttf) format("truetype");
+    }
+    
+    @font-face {
+      font-family: "Jost";
+      font-style: italic;
+      font-weight: normal;
+      font-display: swap;
+      src: url(/assets/fonts/Jost/Jost-Italic-VariableFont_wght.ttf)
+        format("truetype");
+    }
+    
+    @font-face {
+      font-family: "Jost";
+      font-style: normal;
+      font-weight: bold;
+      font-display: swap;
+      src: url(/assets/fonts/Jost/Jost-Bold-VariableFont_wght.ttf)
+        format("truetype");
+    }
+    
+    @font-face {
+      font-family: "Domaine";
+      font-style: normal;
+      font-weight: normal;
+      font-display: swap;
+      src: url(/assets/fonts/Domaine/domaine-display-medium.woff2)
+        format("truetype");
+    }
+    
+    @font-face {
+      font-family: "Domaine";
+      font-style: normal;
+      font-weight: bold;
+      font-display: swap;
+      src: url(/assets/fonts/Domaine/domaine-display-extrabold.woff2)
+        format("truetype");
+    }
+      `}
+  />
+);
