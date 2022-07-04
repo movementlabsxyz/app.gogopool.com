bb8b4761ac6e79c029d038c4a9115b092f41e687
Andre H <andrehadiantolesmana@gmail.com>
2022-07-04 16:43:08 +0800

    feat: implement navigation bar

---
+++ src/modules/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,36 @@
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
+
+import { Button } from "@/common/components/Button";
+
+export const NavigationBar = () => {
+  return (
+    <>
+      <Box
+        p="0.75rem 1.5rem"
+        borderBottomWidth="1px"
+        borderBottomColor="#0000001A"
+        h="58px"
+      >
+        <Flex alignItems="center" justifyContent="space-between">
+          <Box>
+            <Text size="xl" fontWeight="bold">
+              gogopool
+            </Text>
+          </Box>
+
+          <Flex alignItems="center">
+            <Stack direction="row" spacing="7">
+              <Button
+                size="sm"
+                variant="secondary-outline"
+                onClick={() => null}
+              >
+                Connect Wallet
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+};
