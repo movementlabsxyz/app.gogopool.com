76049082524d6e6115ab1d69ecc287e6ee5e9e53
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-07-06 06:23:11 +0700

    📝  doc: create button storybook documentation

---
+++ src/common/components/Button/Button.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+
+import { Button, ButtonProps } from "./Button";
+
+export default {
+  title: "Common/Button",
+  component: Button,
+} as Meta<typeof Button>;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Button",
+  variant: "primary",
+};
+
+export const SecondaryFilled = Template.bind({});
+SecondaryFilled.args = {
+  children: "Button",
+  variant: "secondary-filled",
+};
+
+export const SecondaryOutline = Template.bind({});
+SecondaryOutline.args = {
+  children: "Button",
+  variant: "secondary-outline",
+};
+
+export const DestructiveOutline = Template.bind({});
+DestructiveOutline.args = {
+  children: "Button",
+  variant: "destructive-outline",
+};
