6d27dc1ee0ab7b9559bced91257028adf9bd7522
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-07-06 06:23:24 +0700

    🚨  fix: storybook module not found error

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,14 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 import { withProse } from "@nikolovlazar/chakra-ui-prose";
 
-import { Accordion } from "@/common/components/Accordion";
-
 import { colors } from "./colors";
 import { Button } from "./components/button";
 import { Heading } from "./components/heading";
 import { Text } from "./components/text";
 import { config } from "./config";
 import { fonts } from "./fonts";
+import { Accordion } from "../common/components/Accordion/Accordion";
+
+// NOTE: Please don't use '@/' when import components, it will break the storybook
 
 const theme = extendTheme(
   {
