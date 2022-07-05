bd6dbb5f7724a5e123716461d2c811bec494cb10
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-07-06 06:22:15 +0700

    🔧  chore: setup storybook configuration

---
+++ .storybook/preview.tsx
@@ -0,0 +1,19 @@
+import { Parameters, Story } from "@storybook/react";
+import theme from "../src/theme/index";
+
+export const parameters: Parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  options: {
+    storySort: (a: Story, b: Story) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+  chakra: {
+    theme,
+  },
+};
