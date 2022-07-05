9dc44ef90a167aab4bdfbe8abd1451af0804d658
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-07-06 06:31:54 +0700

    ♻️  refactor: auto sort import

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,13 +1,13 @@
 import { extendTheme } from "@chakra-ui/react";
 import { withProse } from "@nikolovlazar/chakra-ui-prose";
 
+import { Accordion } from "../common/components/Accordion/Accordion";
 import { colors } from "./colors";
 import { Button } from "./components/button";
 import { Heading } from "./components/heading";
 import { Text } from "./components/text";
 import { config } from "./config";
 import { fonts } from "./fonts";
-import { Accordion } from "../common/components/Accordion/Accordion";
 
 // NOTE: Please don't use '@/' when import components, it will break the storybook
 
