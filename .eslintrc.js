1e13048230ebcdd8052e70067da27af0be14df57
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-07-06 06:23:39 +0700

    🔧  chore: add storybook eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:tailwindcss/recommended",
+    "plugin:storybook/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { project: ["./tsconfig.json"] },
