1ea280a43b340d7b38e26578b96b0d3d14118d84
Khang Nguyen <nguyenak95@gmail.com>
2022-07-06 18:07:47 +0700

    add input component (#21)
    * add input component
    
    * update font

---
+++ src/common/components/Input/Input.tsx
@@ -0,0 +1,19 @@
+import { Input as ChakraInput, InputProps } from "@chakra-ui/react";
+
+const DEFAULT_HEIGHT = 42;
+
+export const Input = (props: InputProps) => {
+  return (
+    <ChakraInput
+      style={{ boxShadow: "none" }}
+      fontFamily="Jost"
+      outline={0}
+      _placeholder={{ color: "grey.500" }}
+      _hover={{ borderColor: "grey.300" }}
+      errorBorderColor="error.500"
+      borderRadius="full"
+      height={DEFAULT_HEIGHT}
+      {...props}
+    />
+  );
+};
