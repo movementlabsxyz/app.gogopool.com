61a8a971901511a793bb2f874caed012169473a2
Wahyu Adi Kurniawan <ayungavis@gmail.com>
2022-07-08 04:31:12 +0700

    🚨  fix: convert `div` to `span` element

--- src/modules/components/LiquidStaking/LiquidStaking.tsx
+++ src/modules/components/LiquidStaking/LiquidStaking.tsx
@@ -25,7 +25,7 @@ const statisticData = [
       <>
         Annual Percentage Rate
         <Tooltip placement="right" content="Explanation here">
-          <div className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
+          <span className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
         </Tooltip>
       </>
     ),
@@ -36,7 +36,7 @@ const statisticData = [
       <>
         Exchange Rate
         <Tooltip placement="right" content="Explanation here">
-          <div className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
+          <span className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
         </Tooltip>
       </>
     ),
@@ -59,7 +59,7 @@ const statisticData = [
       <>
         Unstaking Cooldown Period
         <Tooltip placement="right" content="Explanation here">
-          <div className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
+          <span className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
         </Tooltip>
       </>
     ),
@@ -70,7 +70,7 @@ const statisticData = [
       <>
         Redemption Period
         <Tooltip placement="right" content="Explanation here">
-          <div className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
+          <span className="w-3 h-3 ml-1 bg-red-500 rounded-full" />
         </Tooltip>
       </>
     ),
