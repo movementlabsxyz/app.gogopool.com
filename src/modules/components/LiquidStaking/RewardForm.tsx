d6f65819b7b2e3789fe92a304e677c24c3c13811
Andre H <andrehadiantolesmana@gmail.com>
2022-07-12 23:05:00 +0800

    feat: mobile responsive for reward form

--- src/modules/components/LiquidStaking/RewardForm.tsx
+++ src/modules/components/LiquidStaking/RewardForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Divider, Flex, Text } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 
 import { AvalancheIcon } from "@/common/components/CustomIcon/AvalancheIcon";
@@ -11,23 +11,27 @@ interface Props {
 export const RewardForm: FunctionComponent<Props> = ({ reward, balance }) => {
   return (
     <>
-      <Box
-        display="flex"
-        flexDir="row"
+      <Flex
+        flexDir={{ base: "column", sm: "row" }}
         justifyContent="space-between"
-        alignItems="center"
-        mb="2.5"
+        alignItems={{ base: "flex-start", sm: "center" }}
+        mb={{ base: "14px", sm: "2.5" }}
       >
         <Text size="sm" fontWeight="600" color="grey.600">
           RECEIVE ggpAVAX
         </Text>
-        <Box display="flex" flexDir="row" alignItems="center">
+        <Flex flexDir="row" alignItems="center">
           <AvalancheIcon />
           <Text ml="2" size="xxl" fontWeight="bold">
             {reward}
           </Text>
-        </Box>
-      </Box>
+        </Flex>
+      </Flex>
+      <Divider
+        borderColor="grey.300"
+        mb="2"
+        display={{ base: null, sm: "none" }}
+      />
       <Box>
         <Text size="xs" color="grey.600">{`BALANCE: ${balance} GGP-AVAX`}</Text>
       </Box>
