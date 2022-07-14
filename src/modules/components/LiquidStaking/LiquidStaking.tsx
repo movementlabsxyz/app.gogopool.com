085300d64d3fde7d29c737a1ed1e14a1cb8ba826
Andre H <andrehadiantolesmana@gmail.com>
2022-07-14 17:18:54 +0800

    feat: implement mobile view drawer and deposit logic

--- src/modules/components/LiquidStaking/LiquidStaking.tsx
+++ src/modules/components/LiquidStaking/LiquidStaking.tsx
@@ -6,9 +6,12 @@ import {
   AccordionPanel,
   Box,
   FormControl,
+  Hide,
+  Show,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
 
 import { Button } from "@/common/components/Button";
 import { Card, Content, Footer, Title } from "@/common/components/Card";
@@ -18,6 +21,8 @@ import useBalance from "@/hooks/balance";
 import useDeposit from "@/hooks/deposit";
 import useWallet from "@/hooks/wallet";
 
+import { DepositDrawer } from "../Drawer";
+import { DepositModal } from "../Modal";
 import { RewardForm } from "./RewardForm";
 import { StakeForm } from "./StakeForm";
 import { Statistics } from "./Statistics";
@@ -82,15 +87,32 @@ const statisticData = [
 ];
 
 export const LiquidStaking: FunctionComponent = () => {
+  const isMounted = useRef(false);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const { account, activate, provider } = useWallet();
   const balance = useBalance(); // AVAX balance
-  const { send } = useDeposit(provider);
+  const { send, success, isLoading } = useDeposit(provider);
 
   const [amount, setAmount] = useState<number>(); // stake value
   const [reward, setReward] = useState<number>(0); // reward value
+  const [depositStatus, setDepositStatus] = useState<"success" | "failed">(
+    "failed"
+  );
 
   const handleDeposit = async () => {
-    await send(amount);
+    send(amount)
+      .then(() => {
+        if (success) setDepositStatus("success");
+        else setDepositStatus("failed");
+
+        (() => onOpen())();
+      })
+      .catch(() => {
+        setDepositStatus("failed");
+        (() => onOpen())();
+      });
   };
 
   const handleConnect = () => {
@@ -105,75 +127,114 @@ export const LiquidStaking: FunctionComponent = () => {
     setReward(temporaryAmount);
   };
 
+  useEffect(() => {
+    if (isMounted.current) {
+      if (success) setDepositStatus("success");
+      else setDepositStatus("failed");
+      (() => onOpen())();
+    } else {
+      isMounted.current = true;
+    }
+  }, [success]);
+
   return (
-    <Card outer>
-      <Title>Liquid Staking</Title>
-      <Content>
-        <FormControl>
-          <Box position="relative">
-            <Card backgroundColor="grey.100" mb="2">
+    <>
+      <Show above="sm">
+        <DepositModal
+          status={depositStatus}
+          isOpen={isOpen}
+          onClose={onClose}
+          successProps={{
+            amount: amount,
+            token: "AVAX",
+          }}
+        />
+      </Show>
+      <Hide above="sm">
+        <DepositDrawer
+          status={depositStatus}
+          isOpen={isOpen}
+          onClose={onClose}
+          successProps={{
+            amount: amount,
+            token: "AVAX",
+          }}
+        />
+      </Hide>
+      <Card outer>
+        <Title>Liquid Staking</Title>
+        <Content>
+          <FormControl>
+            <Box position="relative">
+              <Card backgroundColor="grey.100" mb="2">
+                <Content>
+                  <StakeForm
+                    amount={amount}
+                    setAmount={setAmount}
+                    setReward={setReward}
+                    balance={balance || 0}
+                  />
+                </Content>
+              </Card>
+              <Box
+                position="absolute"
+                bgColor="green.500"
+                w="8"
+                h="6"
+                borderRadius="md"
+                className="left-[calc(50%-16px)] bottom-[-16px] cursor-pointer"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                onClick={handleSwap}
+              >
+                <SwapIcon size="16px" />
+              </Box>
+            </Box>
+            <Card p="1rem 1.5rem" backgroundColor="grey.100" mb="4">
               <Content>
-                <StakeForm
-                  amount={amount}
-                  setAmount={setAmount}
-                  setReward={setReward}
-                  balance={balance || 0}
-                />
+                <RewardForm reward={reward} balance={0} />
               </Content>
             </Card>
-            <Box
-              position="absolute"
-              bgColor="green.500"
-              w="8"
-              h="6"
-              borderRadius="md"
-              className="left-[calc(50%-16px)] bottom-[-16px] cursor-pointer"
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              onClick={handleSwap}
-            >
-              <SwapIcon size="16px" />
-            </Box>
-          </Box>
-          <Card p="1rem 1.5rem" backgroundColor="grey.100" mb="4">
-            <Content>
-              <RewardForm reward={reward} balance={0} />
-            </Content>
-          </Card>
-          <Card rounded="12px" p="0" backgroundColor="grey.100" mb="2">
-            <Content>
-              <Accordion allowToggle>
-                <AccordionItem>
-                  <AccordionButton
-                    p="1rem 1.5rem"
-                    data-testid="liquid-staking-accordion-action"
-                  >
-                    <Text flex="1" textAlign="left" size="md" fontWeight="bold">
-                      Liquid Staking Statistics
-                    </Text>
-                    <AccordionIcon />
-                  </AccordionButton>
-                  <AccordionPanel p="0 1.5rem 1rem 1.5rem">
-                    <Statistics data={statisticData} />
-                  </AccordionPanel>
-                </AccordionItem>
-              </Accordion>
-            </Content>
-          </Card>
-        </FormControl>
-      </Content>
-      <Footer>
-        {account ? (
-          <Button full disabled={!amount} onClick={handleDeposit}>
-            Deposit
-          </Button>
-        ) : (
-          <Button full onClick={handleConnect}>
-            Connect wallet
-          </Button>
-        )}
-      </Footer>
-    </Card>
+            <Card rounded="12px" p="0" backgroundColor="grey.100" mb="2">
+              <Content>
+                <Accordion allowToggle>
+                  <AccordionItem>
+                    <AccordionButton
+                      p="1rem 1.5rem"
+                      data-testid="liquid-staking-accordion-action"
+                    >
+                      <Text
+                        flex="1"
+                        textAlign="left"
+                        size="md"
+                        fontWeight="bold"
+                      >
+                        Liquid Staking Statistics
+                      </Text>
+                      <AccordionIcon />
+                    </AccordionButton>
+                    <AccordionPanel p="0 1.5rem 1rem 1.5rem">
+                      <Statistics data={statisticData} />
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </Content>
+            </Card>
+          </FormControl>
+        </Content>
+        <Footer>
+          {account ? (
+            <Button full disabled={!amount} onClick={handleDeposit}>
+              {isLoading ? "Loading..." : "Deposit"}
+            </Button>
+          ) : (
+            <Button full onClick={handleConnect}>
+              Connect wallet
+            </Button>
+          )}
+        </Footer>
+      </Card>
+    </>
   );
 };
