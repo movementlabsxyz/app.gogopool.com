099c7daa59c5f291a6329de63d2e2354c52d497a
Andre H <andrehadiantolesmana@gmail.com>
2022-07-14 17:30:01 +0800

    feat: implement storybook

---
+++ src/modules/components/Drawer/FailedDepositDrawer.stories.tsx
@@ -0,0 +1,27 @@
+import { useDisclosure } from "@chakra-ui/react";
+import { Meta, Story } from "@storybook/react";
+
+import { Button } from "@/common/components/Button";
+
+import { DepositDrawer } from "./DepositDrawer";
+import {
+  FailedDepositDrawer,
+  FailedDepositDrawerProps,
+} from "./FailedDepositDrawer";
+
+export default {
+  title: "Modules/FailedDepositDrawer",
+  component: FailedDepositDrawer,
+} as Meta<FailedDepositDrawerProps>;
+
+const Template: Story<FailedDepositDrawerProps> = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Drawer</Button>
+      <DepositDrawer status="failed" isOpen={isOpen} onClose={onClose} />
+    </>
+  );
+};
+
+export const Default = Template.bind({});
