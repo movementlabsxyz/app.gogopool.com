31fb2e357ced1dc5784804426c7902bcceb18786
chand1012 <chandler@chand1012.dev>
2022-07-31 16:42:33 -0400

    Fix storybook build

--- src/common/components/ConnectButton/ConnectButton.stories.tsx
+++ src/common/components/ConnectButton/ConnectButton.stories.tsx
@@ -1,10 +1,18 @@
 import { Meta, Story } from "@storybook/react";
 
+import WagmiDecorator from "../../../stories/decorators/wagmi";
 import ConnectButton from "./ConnectButton";
 
 export default {
   title: "Common/ConnectButton",
   component: ConnectButton,
+  decorators: [
+    (Story) => (
+      <WagmiDecorator>
+        <Story />
+      </WagmiDecorator>
+    ),
+  ],
 } as Meta<typeof ConnectButton>;
 
 const Template: Story = (args) => <ConnectButton {...args} />;
