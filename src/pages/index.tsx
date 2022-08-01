035da62b7e458a921a92661d6b0bb1a8634e76c9
chand1012 <chandler@chand1012.dev>
2022-08-01 15:13:44 -0400

    Add super basic landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,42 @@
+import { useRouter } from "next/router";
+
+import { Button } from "@/common/components/Button";
 import { PageHead } from "@/common/components/PageHead";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="h-full">
-      <PageHead append={false} description="Home page description" name="Home" />
+      <PageHead
+        append={false}
+        description="Home page description"
+        name="Home"
+      />
       <section className="grid h-full place-content-center">
-        <span>Hello world</span>
+        <Button
+          size="md"
+          variant="primary"
+          onClick={() => {
+            router.push("/liquidStaking");
+          }}
+        >
+          {" "}
+          Liquid Staking{" "}
+        </Button>
+        <Button
+          style={{
+            marginTop: "20px",
+          }}
+          size="md"
+          variant="secondary-outline"
+          onClick={() => {
+            router.push("/nodeOperator");
+          }}
+        >
+          {" "}
+          Node Operator{" "}
+        </Button>
       </section>
     </div>
   );
