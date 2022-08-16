da5956fda9b737efb7c330429a94bb7a464bef22
Chandler <chandler@chand1012.dev>
2022-08-15 21:11:01 -0400

    Add landing page redirect (#86)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /* eslint-disable no-undef */
 module.exports = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "https://www.gogopool.com",
+        permanent: true,
+      },
+    ];
+  },
 };
