938da471fe2c013b8b096202a023b1fa7bfcd276
Chandler <chandler@multisiglabs.org>
2022-09-26 10:27:25 -0400

    Update storage address (#118)

--- src/constants/anr.ts
+++ src/constants/anr.ts
@@ -1,2 +1,2 @@
-export const storageAddress = "0xfd47F24A11c3e35ED7E9815C161c180b435697C3";
+export const storageAddress = "0xAE77fDd010D498678FCa3cC23e6E11f120Bf576c";
 export const multicallAddress = "0x52C84043CD9c865236f11d9Fc9F56aa003c1f922";
