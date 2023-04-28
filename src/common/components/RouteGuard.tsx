aac3f8ef81e0fa97d56b0eedbed6eaf166043763
Matt <Geczy@users.noreply.github.com>
2023-04-28 16:16:15 -0500

    Closed Alpha Ready (#91) (#268)

--- src/common/components/RouteGuard.tsx
+++ src/common/components/RouteGuard.tsx
@@ -1,19 +1,3 @@
-import { useEffect } from 'react'
-
-import { useRouter } from 'next/router'
-import { useAccount } from 'wagmi'
-
 export default function RouteGuard() {
-  const router = useRouter()
-  const { isConnected } = useAccount()
-
-  useEffect(() => {
-    if (!isConnected && !router.asPath.includes('/login')) {
-      router.push('/login')
-    } else if (isConnected && router.asPath.includes('/login')) {
-      router.push('/welcome')
-    }
-  }, [isConnected, router])
-
   return null
 }
