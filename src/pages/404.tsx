fa60d43e20c889ff751663b0b344289ea9d283fe
jacktrusler <89369559+jacktrusler@users.noreply.github.com>
2023-08-01 12:10:50 -0400

    404 page (#405)
    New 404 page with some whimsical copy.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,38 @@
-import { Link } from '@chakra-ui/react'
+import { Box, Link, Text } from '@chakra-ui/react'
+
+import notFound from '/public/assets/img/large_assets/404.svg'
+
+import Image from 'next/image'
 
 import contact from '@/common/components/crisp/contact'
+import { SidebarNavbar } from '@/modules/components/SidebarNavbar/SidebarNavbar'
 
 const Error404 = (): JSX.Element => {
   return (
-    <div className="container bg-white">
-      <p className="text-base font-semibold text-gray-800">404: Page not found</p>
-      <br></br>
-      <Link onClick={contact}>Have questions? Connect with our team through our chatbox!</Link>
-    </div>
+    <Box
+      className="flex flex-col items-center justify-center space-y-6 bg-[#F7F9FF] p-8"
+      minH="full"
+    >
+      <div style={{ width: '300px', height: '460px' }}>
+        <Image alt="not found" height={0} src={notFound} width={0} />
+      </div>
+      <div>
+        <Text className="max-w-[500px] text-center font-domaine text-blue-900" fontSize={32}>
+          <span className="text-red-500">404 Error:</span> Page Not Found.
+        </Text>
+        <br></br>
+        <Link onClick={contact}>
+          <Text className="max-w-[500px] text-center font-domaine text-blue-900" fontSize={24}>
+            Questions? Connect though our ChatBox.
+          </Text>
+        </Link>
+      </div>
+    </Box>
   )
+}
+
+Error404.getLayout = function getLayout(page) {
+  return <SidebarNavbar>{page}</SidebarNavbar>
 }
 
 export default Error404
